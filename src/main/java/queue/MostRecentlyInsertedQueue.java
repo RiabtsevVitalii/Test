2bce5b666861dbd32f1aab599a056794f2ca1cbf
Vitaliy Riabtsev <vitaliy.riabtsev@progforce.com>
2016-11-20 19:12:49 +0200

    add remove() method to iterator

--- src/main/java/queue/MostRecentlyInsertedQueue.java
+++ src/main/java/queue/MostRecentlyInsertedQueue.java
@@ -5,9 +5,6 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Queue;
 
-/**
- * Created by VitaliiRiabtsev on 11/16/2016.
- */
 public class MostRecentlyInsertedQueue<E> extends AbstractQueue<E> implements Queue<E> {
 
     private static class Node<E> {
@@ -46,27 +43,70 @@ public class MostRecentlyInsertedQueue<E> extends AbstractQueue<E> implements Qu
         return removedElement;
     }
 
+    void unlink(Node<E> nextNode, Node<E> currentNode) {
+        nextNode.element = null;
+        currentNode.next = nextNode.next;
+        if (tail == nextNode) {
+            tail = currentNode;
+        }
+        count -= 1;
+    }
+
     private class IteratorImpl implements Iterator<E> {
         private Node<E> current;
+        private Node<E> lastRet;
+        private E currentElement;
 
         private IteratorImpl() {
             current = head.next;
+            if (current != null) {
+                currentElement = current.element;
+            }
         }
 
-        @Override
+        private Node<E> nextNode(Node<E> currentNode) {
+            for (; ; ) {
+                Node<E> nextNode = currentNode.next;
+                if (nextNode == currentNode) {
+                    return head.next;
+                }
+                if (nextNode == null || nextNode.element != null) {
+                    return nextNode;
+                }
+                currentNode = nextNode;
+            }
+        }
+
         public boolean hasNext() {
             return current != null;
         }
 
-        @Override
         public E next() {
             if (current == null) {
                 throw new NoSuchElementException();
             }
-
-            E element = current.element;
-            current = current.next;
+            E element = currentElement;
+            lastRet = current;
+            current = nextNode(current);
+            currentElement = (current == null) ? null : current.element;
             return element;
+        }
+
+        public void remove() {
+            if (lastRet == null) {
+                throw new IllegalStateException();
+            }
+
+            Node<E> node = lastRet;
+            lastRet = null;
+            for (Node<E> currentNode = head, nextNode = currentNode.next;
+                 nextNode != null;
+                 currentNode = nextNode, nextNode = nextNode.next) {
+                if (nextNode == node) {
+                    unlink(nextNode, currentNode);
+                    break;
+                }
+            }
         }
     }
 
@@ -87,7 +127,6 @@ public class MostRecentlyInsertedQueue<E> extends AbstractQueue<E> implements Qu
         }
 
         Node<E> node = new Node<>(element);
-
         enqueue(node);
         if (count < capacity) {
             count += 1;
